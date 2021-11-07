867d24b0ac44af52997ded9509a230e4f4d8d073
Robert Kovach <36238115+RobiKovach@users.noreply.github.com>
2021-11-07 16:05:51 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 3,
+  grabCursor: true,
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
